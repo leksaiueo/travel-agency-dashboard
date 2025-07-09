ab997d8bbcec9e3df690aaaabc4b1e1a71d586ac
leksaiueo <57250619+leksaiueo@users.noreply.github.com>
2025-07-09 19:39:01 +0700

    Implement Routing, Dashboard, Layout & UI, Auth Functionlaity

--- app/routes/admin/all-users.tsx
+++ app/routes/admin/all-users.tsx
@@ -1,7 +1,15 @@
-import React from "react";
+import { Header } from "../../../components";
 
 const AllUsers = () => {
-  return <div>Users Table</div>;
+  return (
+    <main className="dashboard wrapper">
+      <Header
+        title="Halaman Trip"
+        description="Check out our current users in real time"
+      />
+      All Users Page Content
+    </main>
+  );
 };
 
 export default AllUsers;
--- app/routes/admin/dashboard.tsx
+++ app/routes/admin/dashboard.tsx
@@ -1,5 +1,61 @@
+import { dashboardStats, user, allTrips } from "~/constants";
+import { Header, TripCard, StatsCard } from "../../../components";
+const { totalUsers, usersJoined, totalTrips, tripsCreated, userRole } =
+  dashboardStats;
+
 const dashboard = () => {
-  return <div>dashboard pages</div>;
+  return (
+    <main className="dashboard wrapper">
+      <Header
+        title={`Welcome ${user?.name ?? "Guest"} 👋 `}
+        description="Track activity, Tren dan destinasi populer secara realtime"
+      />
+
+      <section className="flex flex-col gap-6">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-6 w-full">
+          <StatsCard
+            headerTitle="Total User"
+            total={totalUsers}
+            currentMonthCount={usersJoined.currentMonth}
+            lastMonthCount={usersJoined.lastMonth}
+          />
+          <StatsCard
+            headerTitle="Total Trip"
+            total={totalTrips}
+            currentMonthCount={tripsCreated.currentMonth}
+            lastMonthCount={tripsCreated.lastMonth}
+          />
+          <StatsCard
+            headerTitle="User Aktif"
+            total={userRole.total}
+            currentMonthCount={userRole.currentMonth}
+            lastMonthCount={userRole.lastMonth}
+          />
+        </div>
+      </section>
+      <section className="container">
+        <h1 className="text-xl font-semibold text-dark-100">
+          Yok Bikin Trip Kita
+        </h1>
+        <div className="trip-grid">
+          {allTrips
+            .slice(0, 4)
+            .map(({ id, name, imageUrls, itinerary, tags, estimatedPrice }) => (
+              <TripCard
+                key={id}
+                id={id.toString()}
+                name={name}
+                imageUrl={imageUrls[0]}
+                location={itinerary?.[0]?.location ?? ""}
+                tags={tags}
+                price={estimatedPrice}
+              />
+            ))}
+        </div>
+      </section>
+      <TripCard />
+    </main>
+  );
 };
 
 export default dashboard;
